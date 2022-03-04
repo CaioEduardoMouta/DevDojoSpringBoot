aff92ff458fc23b62e690cc991449036f00c85ad
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-03-04 00:14:06 -0300

    SpringBoot

--- src/test/java/academy/devdojo/springboot2/integration/AnimeControllerIT.java
+++ src/test/java/academy/devdojo/springboot2/integration/AnimeControllerIT.java
@@ -1,10 +1,21 @@
 package academy.devdojo.springboot2.integration;
 
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.AnimeCreator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,5 +28,24 @@ class AnimeControllerIT {
 
     @LocalServerPort
     private int port;
+
+    @Test
+    @DisplayName("list returns list of anime inside page object when successful")
+    void listAll_ReturnasListOfAnimesInsidePageObject_WhenSucessful() {
+        String expectedName = AnimeCreator.createValidAnime().getName();
+
+        PageImpl<Anime> exchange = testRestTemplate.exchange(" /animes", HttpMethod.GET, null,
+                new ParameterizedTypeReference<PageImpl<Anime>>() {
+                }).getBody();
+
+//        Assertions.assertThat(animePage).isNotNull();
+//
+//        Assertions.assertThat(animePage.toList()).isNotEmpty().hasSize(1);
+//
+//        Assertions.assertThat(animePage.toList().get(0).getName()).isEqualTo(expectedName);
+
+
+    }
+
 
 }
